b4c72e83d530c1722393045631c87afdb7051553
Benoit Leroy <lerobeno@ensipc293.imag.fr>
2015-11-30 13:45:15 +0100

    Suite des tests
    Test d'options non existantes
    Test d'options auxquelles on passe une chaine contenant des espaces
    Test de lancement du programme dans différents répertoires

---
+++ tests/lancement.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+# Test du lancement du programme à partir de différents répertoires
+
+HERE=$(cd "$(dirname "$0")" && pwd)
+PATH="$HERE/..:$PATH"
+
+source=$HERE/source
+dest=$HERE/dest
+
+rm -fr $source $dest
+mkdir -p $source $dest
+
+make-img.sh $source/image1.jpg
+make-img.sh $source/image2.jpg
+
+# A partir de l'endroit où on a lancé le test
+galerie-shell.sh --source $source --dest $dest || echo "Test failed"
+
+# A partir de la racine
+cd /
+galerie-shell.sh --source $source --dest $dest || echo "Test failed"
+
+# A partir du répertoire home
+cd
+galerie-shell.sh --source $source --dest $dest || echo "Test failed"
+
+# A partir du répertoire contenant l'exécutable
+cd $HERE
+galerie-shell.sh --source $source --dest $dest || echo "Test failed"
+
+if [ -f $dest/index.html ]
+then
+    echo "Now run 'firefox dest/index.html' to check the result"
+else
+    echo "ERROR: dest/index.html was not generated"
+fi
--- tests/options.sh
+++ tests/options.sh
@@ -10,7 +10,16 @@ make-img.sh source/image1.jpg
 make-img.sh source/image2.jpg
 
 # Tests d'options non existantes
-galerie-shell.sh -source source
-galerie-shell.sh --s source
-galerie-shell.sh
+galerie-shell.sh -source source >/dev/null && echo "Test failed"
+galerie-shell.sh --s source >/dev/null && echo "Test failed"
+galerie-shell.sh verb >/dev/null && echo "Test failed"
+galerie-shell.sh -verb >/dev/null && echo "Test failed"
+galerie-shell.sh --source source -x -y --dest dest >/dev/null && echo "Test failed"
+galerie-shell.sh --source source --dest dest --patate >/dev/null && echo "Test failed"
 
+# Tests options avec paramètres vides
+galerie-shell.sh --source "  " --dest dest --index page.html >/dev/null && echo "Test failed"
+galerie-shell.sh --source source --dest " " --index page.html >/dev/null && echo "Test failed"
+galerie-shell.sh --source source --dest dest --index "" >/dev/null && echo "Test failed"
+
+echo "Fin du test, aucun fichier HTML n'a été généré si tout va bien"
